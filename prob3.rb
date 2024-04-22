73130c81451d869e8753f7e4502c633035ba9266
Yahia Nahhas <yahianahhas15@gmail.com>
2024-04-22 14:10:13 -0500

    Fixed the errors, should work now

--- prob3.rb
+++ prob3.rb
@@ -1,7 +1,8 @@
 require 'httparty'
 require 'json'
 
-def get_events(api_key)
+def get_events(location, api_key)
+  formatted_location = URI.encode_www_form_component(location)
   url = "https://www.eventbriteapi.com/v3/events/search/?token=#{api_key}&expand=venue&location.within=10km&location.latitude=40.7128&location.longitude=-74.0060"  # Adjust the latitude and longitude according to the desired location
   response = HTTParty.get(url)
 
@@ -15,12 +16,13 @@ def get_events(api_key)
 end
 
 api_key = 'U35MEAFDFN667DDXWY'
-events = get_events(api_key)
+location = 'Memphis'  # Replace with the desired location
+events = get_events(location, api_key)
 
 if events.empty?
-  puts "No upcoming events found."
+  puts "No upcoming events found in #{location}."
 else
-  puts "Upcoming events:"
+  puts "Upcoming events in #{location}:"
   events.each_with_index do |event, index|
     puts "#{index + 1}: #{event['name']['text']}"
     puts "Date: #{event['start']['local']}"
